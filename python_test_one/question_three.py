f3663e62287c7377636d1feb963a848c509bb410
Mbabazi-Angel <144022384+Mbabazi-Angel@users.noreply.github.com>
2023-10-19 10:30:26 +0300

    Add files via upload

---
+++ python_test_one/question_three.py
@@ -0,0 +1,19 @@
+#i)
+student_marks = [78,83,90,88,75]
+sum_of_student_marks = sum(student_marks)
+print('The sum of the items in the student marks list is' + ' ' + str(sum_of_student_marks))
+
+#ii)
+print(student_marks[0])
+print(student_marks[4])
+
+#iii)
+new_student_marks = []
+new_student_marks = student_marks.append(96)
+print(student_marks)
+
+#iv)
+#change 78 to 87
+updated_student_marks = []
+updated_student_marks[1] = 87
+print(updated_student_marks)
